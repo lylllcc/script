3523dc3f570e8308b801ace8d5f7ed8723a7ea12
yyljj <yyljj@outlook.com>
2016-04-26 23:26:48 +0800

    明天继续

--- main.py
+++ main.py
@@ -1,15 +1,8 @@
+#coding=utf-8
 from spider import *
-import sys
-reload(sys)
-sys.setdefaultencoding('utf-8')
-if __name__ == '__main__':
-url = 'http://www.jikexueyuan.com/course/?pageNum=1'
-myspider = Spider()
-all_links = mySpider.changePage(url,20)
-for link in all_links:
-    print "正在处理页面" + link
-    html = myspider.getSource(url)
-    everyclass = myspider.getEveryclass(html)
+url = 'http://acm.upc.edu.cn/problemset.php?page=1'
+feng = Spider()
+html = feng.getSource(url) #获取网页源代码。
 
 
 
--- spider.py
+++ spider.py
@@ -1,29 +1,18 @@
 #coding=utf-8
 import re
 import requests
-
-#设置编码
 import sys
 reload(sys)
 sys.setdefaultencoding('utf-8')
-
 class Spider:
     def __init__(self):
-        print '开始抓取。。。。'
-    #获取网页源代码
+        print '开始抓取'
     def getSource(self,url):
-        header = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:45.0) Gecko/20100101 Firefox/45.0'}
-        html = requests.get(url,header = header)
+        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:45.0) Gecko/20100101 Firefox/45.0'}
+        html = requests.get(url,headers = headers)
         html.encoding = 'utf-8'
         return html.text
-    def changPage(self,url,max_page):
-        now_page = int(re.search('pageNum=(\d+)',url,re.S).group(1))
-        page_group = []
-        for i in range(now_page,max_page+1):
-            link = re.sub('pageNum=(\d+)','pageNum=%d'%i,url,re.S)
-            page_group.append(link)
-        return page_group
-    def getEveryclass(self,url):
+
 
 
 
